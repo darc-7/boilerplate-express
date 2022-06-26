e0ddd3b76ddd4cd45221dbacb536d2d818194949
darc-7 <55797130+darc-7@users.noreply.github.com>
2022-06-26 03:43:33 +0000

    Working on Basic Node and Express course. Serving JSON, HTML, statics asset and .env file

--- myApp.js
+++ myApp.js
@@ -1,15 +1,26 @@
 let express = require('express');
 let app = express();
 
+absolutePath = __dirname + '/views/index.html';
 
+console.log('Hello World');
 
+/*app.get('/',function(req, res) {
+  res.send('Hello Express');
+})*/
 
+app.get('/',function(req, res) {
+  res.sendFile(absolutePath);
+})
 
+app.use('/public/', express.static(__dirname + '/public'));
 
-
-
-
-
+app.get("/json", (req, res) => {
+  if (process.env.MESSAGE_STYLE === 'uppercase')
+    res.json({message: "Hello json".toUpperCase()});
+  else
+    res.json({message: "Hello json"})
+  });
 
 
 
